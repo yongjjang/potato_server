c79e5fdd1c9e51e26295a7357b4abbb253ae69c8
yongjjang <pastelhawk@gmail.com>
2020-05-12 02:21:31 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,4 +33,4 @@ def route_frontend(path):
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host="0.0.0.0", debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=8080)
